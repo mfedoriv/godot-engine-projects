29fe775b5f98603ce682aef7701ebaa3410aece3
mfedoriv <mfedorivs@gmail.com>
2023-11-16 02:11:08 +0300

    Snowflake Catch: Add animations to Combo Tracker

--- snowflake-catch/scenes/UI/ComboTracker.gd
+++ snowflake-catch/scenes/UI/ComboTracker.gd
@@ -1,13 +1,16 @@
 extends Panel
 
-@export_range(1, 100) var part_size = 200
-@export var part_size_decreasing_value_per_combo = 10
+@export_range(50, 300) var part_size = 200
+@export var part_size_decreasing_value_per_combo = 18
 @export var decrease_speed = 90
 
 @onready var combo_progress_bar = $Panel/ComboProgressBar
 
 @onready var focus_timer = $FocusTimer
 @onready var combo_label = $Panel/ComboLabel
+@onready var particles_emitting_timer = $ParticlesEmittingTimer
+@onready var combo_particles = $Panel/ComboParticles
+@onready var animation_player = $Panel/AnimationPlayer
 
 var is_focused : bool = true
 var combo = 1
@@ -22,25 +25,36 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print('Is focused:', is_focused)
-	print('Combo:', combo)
-	if combo_progress_bar.value < 2 and combo > 1:
+#	print('Is focused:', is_focused)
+#	print('Combo:', combo)
+	if combo_progress_bar.value < 1 and combo > 1:
 		combo_progress_bar.value = combo_progress_bar.max_value
 		combo -= 1
 		update_combo_label()
+		animation_player.play("ComboDown")
 	if not is_focused:
-		print('Progress Bar Value:', combo_progress_bar.value)
+#		print('Progress Bar Value:', combo_progress_bar.value)
 #		print('Progress Bar Decreasing step:', decrease_speed * delta)
 		combo_progress_bar.value -= decrease_speed * delta
+	particles_emmiting()
+		
+		
 
 func add_part():
-	var new_value = combo_progress_bar.value + max(part_size / combo, part_size - (combo * 10))
+	var new_value = combo_progress_bar.value + max(part_size / combo, part_size - (combo * part_size_decreasing_value_per_combo))
 	if new_value >= combo_progress_bar.max_value:
 		combo += 1
 		update_combo_label()
+		animation_player.play("ComboUp")
+		particles_emitting_timer.start()
 	combo_progress_bar.value = int(new_value) % int(combo_progress_bar.max_value)
 	is_focused = true
 	focus_timer.start()
+
+func particles_emmiting():
+	combo_particles.emitting = not particles_emitting_timer.is_stopped()
+	
+
 
 func update_combo_label():
 	print('\t\tCombo:', combo)
