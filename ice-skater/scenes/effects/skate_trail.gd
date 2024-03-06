9b2fda5f14f83b21b6262b7c32945148a31286d4
mfedoriv <mfedorivs@gmail.com>
2024-03-06 15:00:25 +0300

    Added Project Template

--- ice-skater/scenes/effects/skate_trail.gd
+++ ice-skater/scenes/effects/skate_trail.gd
@@ -1,11 +1,11 @@
 extends Line2D
 
 @export_category('Trail')
-@export var length : = 10
-@export var offset : = Vector2.ZERO
+@export var length := 10
+@export var offset := Vector2.ZERO
 @export var is_right: bool
 
-@onready var parent : Node2D = get_parent()
+@onready var parent: Node2D = get_parent()
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 var can_draw: bool = true
@@ -13,35 +13,23 @@ var can_draw: bool = true
 signal screen_exited_line
 
 func _ready() -> void:
-	#visible_on_screen_notifier_2d.screen_exited.connect(delete_line)
-	print(self)
-	#is_right = parent.is_right
-	#offset = position
-	#offset.x += 25 if is_right else -25
-	#top_level = true
+	visible_on_screen_notifier_2d.screen_exited.connect(delete_line)
+	is_right = parent.is_right
+	offset = position
+	offset.x += 25 if is_right else -25
 	width = 2
 
-func _process(_delta: float) -> void:
-	global_position = Vector2.ZERO
-	draw_point()
-		
-
 func draw_point():
-	#if !can_draw:
-		#return
-	#if is_right != parent.is_right:
-		#can_draw = false
-		#return
-	#print('Is Right: ', is_right, ' Parent Is Right: ', parent.is_right)
-	# Перемещаем линию в соответствии с позицией родителя
-	var point : = global_position + offset
+	if !can_draw:
+		return
+	if is_right != parent.is_right:
+		can_draw = false
+		return
+	var point: Vector2 = parent.global_position + offset
 	add_point(point, 0)
-
-
-#func delete_line():
-	#print("Deleting line")
-	#queue_free()
-
 
 func _on_screen_exited() -> void:
 	screen_exited_line.emit()
+
+func delete_line() -> void:
+	queue_free()
